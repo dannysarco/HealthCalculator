8dfa885e7dbe2207063b55b00be63b0c23246097
Danny Sarco <54184032+dannysarco@users.noreply.github.com>
2024-06-10 12:41:36 -0400

    Initial Commit

---
+++ HealthCalculator/HealthCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthCalculatorApp.swift
+//  HealthCalculator
+//
+//  Created by Daniel Sarco on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
